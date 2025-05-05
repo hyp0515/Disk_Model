1c6a496480b641574a4bdbecc6a70b2a0a95fc8d
hyp0515 <pinkie0515@gmail.com>
2025-05-06 01:41:51 +0800

    Modify interp2d to RegularGridInterpolator

--- X22_model/disk_model.py
+++ X22_model/disk_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import warnings
-import optool
+from . import optool
 import os
 
 """
@@ -41,16 +41,28 @@ class interp1d_log:
     def __call__(self, x):
         return np.exp(self.f_log(np.log(x)))
 # for 2d interp, need z.shape = (len(y), len(x))
+# class interp2d_semi_log_scalar:
+#     def __init__(self, x, y, z):
+#         self.f_log = scipy.interpolate.interp2d(x,np.log(y),np.log(z))
+#     def __call__(self, x, y):
+#         return  np.exp(self.f_log(x,np.log(y))[0])
+# class interp2d_log_vectorized:
+#     def __init__(self, x, y, z):
+#         self.f_log = np.vectorize(scipy.interpolate.interp2d(np.log(x),np.log(y),np.log(z)))
+#     def __call__(self, x, y):
+#         return  np.exp(self.f_log(np.log(x),np.log(y)))
 class interp2d_semi_log_scalar:
     def __init__(self, x, y, z):
-        self.f_log = scipy.interpolate.interp2d(x,np.log(y),np.log(z))
+        self.f_log = scipy.interpolate.RegularGridInterpolator((x,np.log(y)),np.log(z.T))
     def __call__(self, x, y):
-        return  np.exp(self.f_log(x,np.log(y))[0])
+        xx, yy = np.meshgrid(x, y)
+        return  np.exp(self.f_log((xx,np.log(yy)))[0])
 class interp2d_log_vectorized:
     def __init__(self, x, y, z):
-        self.f_log = np.vectorize(scipy.interpolate.interp2d(np.log(x),np.log(y),np.log(z)))
+        self.f_log = np.vectorize(scipy.interpolate.RegularGridInterpolator((np.log(x),np.log(y)),np.log(z.T)))
     def __call__(self, x, y):
-        return  np.exp(self.f_log(np.log(x),np.log(y)))
+        xx, yy = np.meshgrid(x, y)
+        return  np.exp(self.f_log((np.log(xx),np.log(yy))))
 # planck function
 def B(T,nu):
     return 2*h*nu**3/c_light**2 * 1/(np.exp(h*nu/(kB*T))-1)
@@ -161,7 +173,7 @@ def generate_opacity_table_x22(
             grain_properties = pickle.load(f)
     except FileNotFoundError:
         try:
-            with open('../../'+precomputed_grain_properties_fname, "rb") as f:
+            with open('./X22_model/'+precomputed_grain_properties_fname, "rb") as f:
                 grain_properties = pickle.load(f)
         except FileNotFoundError:
             compute_grain_properties_DSHARP(fname='./'+precomputed_grain_properties_fname)
--- radmc/spherical_x22.py
+++ radmc/spherical_x22.py
@@ -1,6 +1,6 @@
 import numpy as np
 from X22_model.disk_model import *
-from scipy.interpolate import interp2d
+from scipy.interpolate import RegularGridInterpolator
 from scipy.optimize import minimize_scalar
 # All unit are in cgs except height and radius are in au
 #########################################################################################
@@ -235,13 +235,21 @@ class DiskModel_spherical:
         i                : index of r_sph
         j                : index of theta_sph
         '''
+        # def interpolate(data_map):  
+        #     interpolator = interp2d(self.Z_grid, self.R_grid, data_map, kind='linear')
+        #     interpolated_data = np.empty((self.NR, self.NTheta//2))
+        #     for r in range(self.NR):
+        #         for theta in range(self.NTheta//2): 
+        #             interpolated_data[r, theta] = interpolator(z_sph_in_cyl[r, theta], r_sph_in_cyl[r, theta])            
+        #     return interpolated_data
         def interpolate(data_map):  
-            interpolator = interp2d(self.Z_grid, self.R_grid, data_map, kind='linear')
+            interpolator = RegularGridInterpolator((self.Z_grid, self.R_grid), data_map.T, bounds_error=False, fill_value=None)
             interpolated_data = np.empty((self.NR, self.NTheta//2))
             for r in range(self.NR):
-                for theta in range(self.NTheta//2): 
-                    interpolated_data[r, theta] = interpolator(z_sph_in_cyl[r, theta], r_sph_in_cyl[r, theta])            
-            return interpolated_data    
+                for theta in range(self.NTheta//2):
+                    point = [z_sph_in_cyl[r, theta], r_sph_in_cyl[r, theta]]
+                    interpolated_data[r, theta] = interpolator(point)          
+            return interpolated_data     
         def mirror_with_r_plane(map):
             map_mirror = np.fliplr(map)            
             return np.concatenate((map, map_mirror), axis= 1)        
